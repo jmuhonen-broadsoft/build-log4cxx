13044a4c2eb4bb8ba8315739fcfa6643a523a740
Jussi Muhonen <jmuhonen@broadsoft.com>
2012-06-11 17:57:12 +0300

    now it builds with ant

--- build.cmd
+++ build.cmd
@@ -1,5 +1,4 @@
 @echo off
-perl apr\build\cvtdsp.pl -2005
-cd apr-util
-devenv aprutil.dsw /MAKE libapr - Win32 Release" libapriconv - Win32 Release" gen_uri_delims - Win32 Release" xml - Win32 Release" libaprutil - Win32 Release"
+cd log4cxx
+call ant %* -lib ..\libraries\cpptasks-1.0b5.jar -lib ..\libraries\xercesImpl.jar
 cd ..
