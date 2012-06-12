b36916d49d110db280d570c73e1e11a627723ac9
Jussi Muhonen <jmuhonen@broadsoft.com>
2012-06-12 13:23:11 +0300

    fix slashes

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set +v
 cd log4cxx
-source ant $* -lib ..\libraries\cpptasks-1.0b5.jar -lib ..\libraries\xercesImpl.jar
+source ant $* -lib ../libraries/cpptasks-1.0b5.jar -lib ../libraries/xercesImpl.jar
 cd ..
