27bbc880b59da5ba36efbb5ee00561cd2538e1a4
Jussi Muhonen <jmuhonen@broadsoft.com>
2012-06-14 16:22:45 +0300

    enhanced exporting, may be called with building

--- build-rel.cmd
+++ build-rel.cmd
@@ -2,5 +2,7 @@
 call git submodule update --init
 call .\clean.cmd
 cd log4cxx
-call ant %* -lib ..\libraries\cpptasks-1.0b5.jar -lib ..\libraries\xercesImpl.jar -Ddebug=false
+call ant -lib ..\libraries\cpptasks-1.0b5.jar -lib ..\libraries\xercesImpl.jar -Ddebug=false
 cd ..
+
+IF NOT [%1] == [] call .\export.cmd release %1
--- export.cmd
+++ export.cmd
@@ -7,17 +7,16 @@ goto error
 :debug
 xcopy log4cxx\target\debug\shared\log4cxx.dll %2\extern\prebuild\Debug\win32\bin\ /Y /I
 xcopy log4cxx\target\debug\shared\log4cxx.lib %2\extern\prebuild\Debug\win32\lib\ /Y /I
+rmdir %2\extern\prebuild\Debug\win32\include\log4cxx /S /Q
 xcopy log4cxx\src\main\include\log4cxx        %2\extern\prebuild\Debug\win32\include\log4cxx\ /S /Y /I
-goto end
+goto:eof
 
 :release
 xcopy log4cxx\target\release\shared\log4cxx.dll %2\extern\prebuild\Release\win32\bin\ /Y /I
 xcopy log4cxx\target\release\shared\log4cxx.lib %2\extern\prebuild\Release\win32\lib\ /Y /I
-xcopy log4cxx\src\main\include\log4cxx          %2\extern\prebuild\Release\win32\include\ /S /Y /I
-goto end
+rmdir %2\extern\prebuild\Release\win32\include\log4cxx /S /Q
+xcopy log4cxx\src\main\include\log4cxx          %2\extern\prebuild\Release\win32\include\log4cxx /S /Y /I
+goto:eof
 
 :error
 echo Usage: export debug/release pc-client-git-root
-
-:end
-@echo on
